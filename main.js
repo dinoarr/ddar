84bc44295a55b31fd2226ae486683d007877f898
dinoarr <dinoananda.restyanto@gmail.com>
2023-10-26 23:05:36 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+new Typed('#typed',{
+    strings : ['Student Of SMKN 4 Tangerang','Web Developer'],
+    typeSpeed : 60,
+    backDelay: 2500,
+    loop : true,
+    backSpeed: 60,
+    startDelay: 1500,
+  });
+
+AOS.init();
